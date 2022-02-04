d9589b4f9df27175cfc9a7f056bef1e8c7e26131
Pol <polgarciamoreno392@gmail.com>
2022-02-04 19:22:20 +0100

    xd

--- resources/views/restaurante.blade.php
+++ resources/views/restaurante.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>INICIO</title>    
+    <title>RESTAURANTE</title>    
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script defer src="css/fontawesome/js/all.js"></script>
 
 </head>
 <body>
     <header>
-        <div class="header">
+        <div class="header1">
             <div class="logo">
                     <img class="logoimagen" src="img/logo.png">
             </div>
@@ -20,6 +20,38 @@
             </div>
         </div>
     </header>
+    <div class="main">
+        <div class="menu">
+            <ul>
+                <li><a href="">Fotos</a></li>
+                <li><a href="">Descripción</a></li>
+                <li><a href="">Ubicación y contacto</a></li>
+                <li><a href="">Opiniones</a></li>
+            </ul>
+        </div>
+        <div class="info">
+            <div class="titulores">
+                <h1>Top text</h1>
+            </div>
+            <div class="descripcion">
+                <h4>Bottom text</h4>
+            </div>
+        </div>
+    </div>
+    <div class="contenido">
+        <div class="fotos">
+            <h1>TEXTO DE MUESTRA</h1>
+        </div>
+        <div class="detalles">
+            <h1>TEXTO DE MUESTRA</h1>
+        </div>
+        <div class="ubicacion">
+            <h1>TEXTO DE MUESTRA</h1>
+        </div>
+        <div class="opiniones">
+            <h1>TEXTO DE MUESTRA</h1>
+        </div>
+    </div>
     <footer>
         <div class="footer">
             <p>Creado por Juan Carlos, Pol y Gerard</p>
