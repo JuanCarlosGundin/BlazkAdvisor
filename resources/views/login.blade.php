9cc23a988009d2da822927c09b1c42fb99a76407
100007217 <gomezmonterroso14@gmail.com>
2022-02-04 15:33:50 +0100

    Pre merge con pol

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script defer src="css/fontawesome/js/all.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -52,6 +53,128 @@
     </div>
     
     <div id="errores_reg"></div>
+    <!-- Hasta aqui -->
+    <header>
+        <div class="header">
+            <div class="logo">
+                    <img class="logoimagen" src="img/logo.png">
+            </div>
+            <!-- Inicio de sesión -->
+            <div class="inicio">
+                <button class="iniciosesion"><b>Iniciar sesión</b></button>
+            </div>
+        </div>
+    </header>
+
+    <div class="main">
+        <div class="tipos">
+            <div class="botonestipos">
+                <button class="tiposcomida"><b>Comida Italiana</b>
+                    <div class="iconos">
+                        <i class="fas fa-pizza-slice"></i>
+                    </div>
+                </button>
+            </div>
+            <div class="botonestipos">
+                <button class="tiposcomida"><b>Comida Española</b>
+                    <div class="iconos">
+                        <i class="fas fa-utensils"></i>
+                    </div>
+                </button>
+            </div>
+            <div class="botonestipos">
+                <button class="tiposcomida"><b>Comida Mexicana</b>
+                    <div class="iconos">
+                        <i class="fas fa-pepper-hot"></i>
+                    </div>
+                </button>
+            </div>
+            <div class="botonestipos">
+                <button class="tiposcomida"><b>Comida arabe</b>
+                    <div class="iconos">
+                        <i class="fas fa-mosque"></i>
+                    </div>
+                </button>
+            </div>
+            <div class="botonestipos">
+                <button class="tiposcomida"><b>Comida china</b><div class="iconos">
+                    <i class="far fa-star"></i>
+                </div>
+            </button>
+            </div>
+        </div>
+        <div class="filtro">
+            <div class="imagenfiltro">
+                <div class="filtroinput">
+                    <i class="fas fa-search lupa"></i>
+                    <input class="input1" type="text" autocomplete="off" autocorrect="off" autocapitalize="off" placeholder="¿Donde quieres comer?">
+                </div>
+            </div>
+        </div>
+        <div class="restaurantes">
+            <h1>Restaurantes</h1>
+            <h4>Los mejores restaurantes de la ciudad</h4>
+                <div class="restaurante">
+                        <button class="resbtn">
+                            <div class="imagen">
+                                <p>IMAGEN</p>
+                            </div>
+                            <div class="titulo">
+                                <p>TITULO DE MUESTRA</p>
+                            </div>
+                            <div class="estrellas">
+                                <p>ESTRELLAS</p>
+                            </div>
+                            <div class="desc">
+                                <p>DESCRIPCION</p>
+                            </div>
+                        </button>
+                        <button class="resbtn">
+                            <div class="imagen">
+                                <p>IMAGEN</p>
+                            </div>
+                            <div class="titulo">
+                                <p>TITULO DE MUESTRA</p>
+                            </div>
+                            <div class="estrellas">
+                                <p>ESTRELLAS</p>
+                            </div>
+                            <div class="desc">
+                                <p>DESCRIPCION</p>
+                            </div>
+                        </button>
+                        <button class="resbtn">
+                            <div class="imagen">
+                                <p>IMAGEN</p>
+                            </div>
+                            <div class="titulo">
+                                <p>TITULO DE MUESTRA</p>
+                            </div>
+                            <div class="estrellas">
+                                <p>ESTRELLAS</p>
+                            </div>
+                            <div class="desc">
+                                <p>DESCRIPCION</p>
+                            </div>
+                        </button>
+                        <button class="resbtn">
+                            <div class="imagen">
+                                <p>IMAGEN</p>
+                            </div>
+                            <div class="titulo">
+                                <p>TITULO DE MUESTRA</p>
+                            </div>
+                            <div class="estrellas">
+                                <p>ESTRELLAS</p>
+                            </div>
+                            <div class="desc">
+                                <p>DESCRIPCION</p>
+                            </div>
+                        </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 <script src="./js/script.js"></script>
 </html>
